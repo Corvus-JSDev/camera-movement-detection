639104167b2b355c289f797c8e555d268c499069
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-28 17:35:45 -0400

    add green box around new objects

--- main.py
+++ main.py
@@ -38,17 +38,32 @@ pp(frame5)
 
 first_frame = None
 while True:
+	#* Grab the frame from the camera
 	check, frame = video.read()
-	# Convert the frame to grayscale and add some blur to make it easier on the system
+
+	#* Convert the frame to grayscale and add some blur to make it easier on the system
 	gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 	gray_frame_blur = cv.GaussianBlur(gray_frame, (11, 11), 0)  # Note: The blur value must be an odd number
-	cv.imshow("My Video", gray_frame_blur)
 
+	#* Save the first frame for a reference to see what has changed
 	if first_frame is None:
 		first_frame = gray_frame_blur
-
 	delta_frame = cv.absdiff(first_frame, gray_frame_blur)
+
+	#* Convert to pure black or white
 	thresh_frame = cv.threshold(delta_frame, 30, 255, cv.THRESH_BINARY)[1]  # Any value >30 will be pure white, <30 will be pure black
+	dil_frame = cv.dilate(thresh_frame, None, iterations=2)
+
+	#* Add a green box around new objects
+	contours, check = cv.findContours(dil_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+	for contour in contours:
+		if not cv.contourArea(contour) < 10_000:
+			x, y, w, h = cv.boundingRect(contour)
+			cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	#* Show the video
+	cv.imshow("My Video", frame)
 
 	# Quit if the q key is pressed
 	key = cv.waitKey(1)
