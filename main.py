80cf3dd4bdced2dd98a08f61587e3f33fb79a4e6
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-28 16:02:52 -0400

    import opencv

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from pprint import pp
+import cv2 as cv  # Note: cv2 uses BGR instead of RGB
 
-# cv2 uses BGR instead of RGB
+# Note: Just capturing a single frame will likely return a black screen as the camera itself doesnt have time to turn on
+video = cv.VideoCapture(0)
+check, frame = video.read()
+
+print(check)  # This will check if the camera as turned on
+print(frame)  # This will print the BGR value of the frame
+
+# Grabbing multiple frames
